7269bc02b7d1b99addc6cb56056fef89a5078862
robsonlinhares <8KWPDuVTe3RSPxB>
2022-01-27 12:40:19 -0300

    Add projeto

---
+++ LeituraArquivos/Program.cs
@@ -0,0 +1,21 @@
+﻿const string filePath = @"C:\Labs\arquivo.txt";
+
+//var data = File.ReadAllText(filePath);
+//Console.WriteLine(data);
+
+//var data = File.ReadAllLines(filePath);
+//var count = 0;
+
+//foreach (var line in data)
+//{
+//    count++;
+//    Console.WriteLine($"Linha: {count} - {line}");
+//}
+
+using var file = new StreamReader(filePath);
+string? line;
+
+while ((line = file.ReadLine()) != null)
+    Console.WriteLine(line);
+
+file.Close();
